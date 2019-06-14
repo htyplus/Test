37c6bd2ffaa0df65708205eea1e5c9d6fc8a35c9
Htet Thu Ya Aung <hplus@localhost.localdomain>
2019-06-14 17:45:45 +0630

    added menu.php

---
+++ menu.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <a href="http://www.htetthuya.com/default.php">Home</a>
+        <a href="http://www.htetthuya.com/Aboutme.php">AboutMe</a>
+        <a href="http://www.htetthuya.com/content.php">contentMe</a>
+        
+        
+    </body>
+</html>
